7e0e0e5ff2a9f21f916ed9c57ae3348e7ef94d8c
abraham mathew <abraham.mathew@seqato.com>
2018-04-25 15:36:52 +0530

    changes in forgot password

--- app/src/main/java/com/beachpartnerllc/beachpartner/activity/LoginActivity.java
+++ app/src/main/java/com/beachpartnerllc/beachpartner/activity/LoginActivity.java
@@ -451,48 +451,49 @@ public class LoginActivity extends AppCompatActivity {
                                         intent.putExtra("reDirectPage","home");
                                         startActivity(intent);
                                         finish();
-                                    }else {
-                                        //Whether user want to update his/her profile o
-                                        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                                        builder.setMessage("Please take a moment to complete your profile. Your profile will remain private and hidden from the world until you complete it.");
-                                        builder.setCancelable(true);
-
-                                        builder.setPositiveButton(
-                                                "OK",
-                                                new DialogInterface.OnClickListener() {
-                                                    public void onClick(DialogInterface dialog, int id) {
-                                                        dialog.cancel();
-                                                        //Goto profile fragment
-                                                        /*ProfileFragment pfragment = new ProfileFragment();
-                                                        FragmentManager mang = getSupportFragmentManager();
-                                                        FragmentTransaction transaction = mang.beginTransaction();
-                                                        transaction.add(R.id.container,pfragment);
-                                                        transaction.commit();*/
-                                                        Intent intent = new Intent(LoginActivity.this,TabActivity.class);
-                                                        intent.putExtra("reDirectPage","profile");
-                                                        startActivity(intent);
-                                                        finish();
-
-                                                    }
-                                                });
-
-                                        builder.setNegativeButton(
-                                                "Later",
-                                                new DialogInterface.OnClickListener() {
-                                                    public void onClick(DialogInterface dialog, int id) {
-                                                        dialog.cancel();
-                                                        //Goto home fragment
-                                                        Intent intent = new Intent(LoginActivity.this,TabActivity.class);
-                                                        intent.putExtra("reDirectPage","home");
-                                                        startActivity(intent);
-                                                        finish();
-                                                    }
-                                                });
-
-                                        AlertDialog alert = builder.create();
-                                        alert.show();
-
                                     }
+//                                    else{
+//                                        //Whether user want to update his/her profile o
+//                                        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+//                                        builder.setMessage("Please take a moment to complete your profile. Your profile will remain private and hidden from the world until you complete it.");
+//                                        builder.setCancelable(true);
+//
+//                                        builder.setPositiveButton(
+//                                                "OK",
+//                                                new DialogInterface.OnClickListener() {
+//                                                    public void onClick(DialogInterface dialog, int id) {
+//                                                        dialog.cancel();
+//                                                        //Goto profile fragment
+//                                                        /*ProfileFragment pfragment = new ProfileFragment();
+//                                                        FragmentManager mang = getSupportFragmentManager();
+//                                                        FragmentTransaction transaction = mang.beginTransaction();
+//                                                        transaction.add(R.id.container,pfragment);
+//                                                        transaction.commit();*/
+//                                                        Intent intent = new Intent(LoginActivity.this,TabActivity.class);
+//                                                        intent.putExtra("reDirectPage","profile");
+//                                                        startActivity(intent);
+//                                                        finish();
+//
+//                                                    }
+//                                                });
+//
+//                                        builder.setNegativeButton(
+//                                                "Later",
+//                                                new DialogInterface.OnClickListener() {
+//                                                    public void onClick(DialogInterface dialog, int id) {
+//                                                        dialog.cancel();
+//                                                        //Goto home fragment
+//                                                        Intent intent = new Intent(LoginActivity.this,TabActivity.class);
+//                                                        intent.putExtra("reDirectPage","home");
+//                                                        startActivity(intent);
+//                                                        finish();
+//                                                    }
+//                                                });
+//
+//                                        AlertDialog alert = builder.create();
+//                                        alert.show();
+//
+//                                    }
 
 
 
