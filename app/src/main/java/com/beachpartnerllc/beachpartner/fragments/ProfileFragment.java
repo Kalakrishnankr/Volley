34f03669998b26e952b0b13e9b7d8a289b662139
seq-kala <rp@seqato.com>
2018-05-01 12:41:08 +0530

    Merge branch 'master' of https://bitbucket.org/samztephen/beachpartnerandroid
    # Conflicts:
    #	.idea/libraries/com_android_support_multidex_1_0_2.xml
    #	.idea/libraries/com_android_support_multidex_instrumentation_1_0_2.xml
    #	app/app.iml

--- app/src/main/java/com/beachpartnerllc/beachpartner/fragments/ProfileFragment.java
+++ app/src/main/java/com/beachpartnerllc/beachpartner/fragments/ProfileFragment.java
@@ -613,8 +613,14 @@ public class ProfileFragment extends Fragment implements View.OnClickListener, A
                     Intent intent = new Intent(Intent.ACTION_SEND);
                     intent.putExtra(Intent.EXTRA_TEXT, "https://www.beachpartner.com/preregistration/");
                     intent.putExtra(Intent.EXTRA_SUBJECT, "BeachPartner App");
-                    intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(myProfileImageFile));
+                   // intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(myProfileImageFile));
                     intent.setType("image/*");
+                    Uri apkURI = FileProvider.getUriForFile(
+                            getContext(),
+                            getContext().getPackageName()+".provider", myProfileImageFile);
+                    intent.putExtra(Intent.EXTRA_STREAM, apkURI);
+                   // intent.setDataAndType(apkURI, "image/*");
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     startActivity(Intent.createChooser(intent, "Share image via..."));
 
 
@@ -641,12 +647,12 @@ public class ProfileFragment extends Fragment implements View.OnClickListener, A
                     intent.setType("video/*");
                  //   intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(myProfileVideFile)); // deprecated
                     Uri apkURI = FileProvider.getUriForFile(
-                            getActivity(),
-                            getActivity().getApplicationContext()
-                                    .getPackageName() + ".provider", myProfileVideFile);
-                    intent.setDataAndType(apkURI, "video/*");
+                            getContext(),
+                            getContext().getPackageName()+".provider", myProfileVideFile);
+                    intent.putExtra(Intent.EXTRA_STREAM, apkURI);
+                    // intent.setDataAndType(apkURI, "video/*");
                     intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    startActivity(Intent.createChooser(intent, "Share image via..."));
+                    startActivity(Intent.createChooser(intent, "Share video via..."));
 
 
                        /* ShareLinkContent contentvideo = new ShareLinkContent.Builder()
@@ -2309,8 +2315,14 @@ public class ProfileFragment extends Fragment implements View.OnClickListener, A
 
                 if (data.hasExtra("data")) {
                     profilePhoto = (Bitmap) data.getExtras().get("data");
+                    try {
+                        profilePhoto = handleSamplingAndRotationBitmap(getContext(),getImageUri(getApplicationContext(),profilePhoto));
+                    } catch (Exception e) {
+                        Log.e(TAG,"COVERT_ERROR");
+                    }finally {
+                        selectedImageUri = getImageUri(getApplicationContext(), profilePhoto);
+                    }
 
-                    selectedImageUri = getImageUri(getApplicationContext(), profilePhoto);
                 } else {
                     // CALL THIS METHOD TO GET THE URI FROM THE BITMAP
 
@@ -2395,7 +2407,109 @@ public class ProfileFragment extends Fragment implements View.OnClickListener, A
 
         return fileSizeInMB;
     }
+    public static Bitmap handleSamplingAndRotationBitmap(Context context, Uri selectedImage)
+            throws IOException {
+        int MAX_HEIGHT = 1024;
+        int MAX_WIDTH = 1024;
 
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        InputStream imageStream = context.getContentResolver().openInputStream(selectedImage);
+        BitmapFactory.decodeStream(imageStream, null, options);
+        imageStream.close();
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        imageStream = context.getContentResolver().openInputStream(selectedImage);
+        Bitmap img = BitmapFactory.decodeStream(imageStream, null, options);
+
+        img = rotateImageIfRequired(context, img, selectedImage);
+        return img;
+    }
+
+    /**
+     * Calculate an inSampleSize for use in a {@link BitmapFactory.Options} object when decoding
+     * bitmaps using the decode* methods from {@link BitmapFactory}. This implementation calculates
+     * the closest inSampleSize that will result in the final decoded bitmap having a width and
+     * height equal to or larger than the requested width and height. This implementation does not
+     * ensure a power of 2 is returned for inSampleSize which can be faster when decoding but
+     * results in a larger bitmap which isn't as useful for caching purposes.
+     *
+     * @param options   An options object with out* params already populated (run through a decode*
+     *                  method with inJustDecodeBounds==true
+     * @param reqWidth  The requested width of the resulting bitmap
+     * @param reqHeight The requested height of the resulting bitmap
+     * @return The value to be used for inSampleSize
+     */
+    private static int calculateInSampleSize(BitmapFactory.Options options,
+                                             int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee a final image
+            // with both dimensions larger than or equal to the requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+
+            // This offers some additional logic in case the image has a strange
+            // aspect ratio. For example, a panorama may have a much larger
+            // width than height. In these cases the total pixels might still
+            // end up being too large to fit comfortably in memory, so we should
+            // be more aggressive with sample down the image (=larger inSampleSize).
+
+            final float totalPixels = width * height;
+
+            // Anything more than 2x the requested pixels we'll sample down further
+            final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+
+            while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+                inSampleSize++;
+            }
+        }
+        return inSampleSize;
+    }
+
+    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
+
+        InputStream input = context.getContentResolver().openInputStream(selectedImage);
+        ExifInterface ei;
+        if (Build.VERSION.SDK_INT > 23)
+            ei = new ExifInterface(input);
+        else
+            ei = new ExifInterface(selectedImage.getPath());
+
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
+    }
+
+    private static Bitmap rotateImage(Bitmap img, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+        img.recycle();
+        return rotatedImg;
+    }
 
 /*
 *  Methods for convert the camera image
