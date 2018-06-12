166323b9bfe8b39678e005c2fd3c82dfff169624
Samuel Robert <samuel.robert@seqato.com>
2018-06-12 16:13:03 +0530

    feedback: Fixes file picker issues

--- app/src/main/java/com/beachpartnerllc/beachpartner/fragments/FeedBackFragment.java
+++ app/src/main/java/com/beachpartnerllc/beachpartner/fragments/FeedBackFragment.java
@@ -1,219 +1,120 @@
 package com.beachpartnerllc.beachpartner.fragments;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.beachpartnerllc.beachpartner.R;
 import com.beachpartnerllc.beachpartner.activity.TabActivity;
 import com.beachpartnerllc.beachpartner.connections.ApiService;
 
+import static android.app.Activity.RESULT_OK;
+
 
 public class FeedBackFragment extends Fragment {
-
-    private WebView webview;
-    private ProgressBar progressBar;
-    private TabActivity tabActivity;
-    private ValueCallback<Uri> mUploadMessage;
-    public ValueCallback<Uri[]> uploadMessage;
-    public static final int REQUEST_SELECT_FILE = 100;
-    private final static int FILECHOOSER_RESULTCODE = 1;
-
-
-    public FeedBackFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_feed_back, container, false);
-        webview     = (WebView) view.findViewById(R.id.webviews);
-        progressBar = (ProgressBar) view.findViewById(R.id.pgbars);
-
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        if (getActivity() instanceof TabActivity) {
-            tabActivity= (TabActivity)getActivity();
-            tabActivity.setActionBarTitle("Feedback");
-        }
-
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setSupportZoom(true);
-        webview.getSettings().setBuiltInZoomControls(true);
-        webview.loadUrl(ApiService.FEEDBACK_PAGE);
-        progressBar.setMax(100);
-        progressBar.setProgress(1);
-
-        webview.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-                progressBar.setProgress(progress);
-            }
-        });
-        webview.setWebViewClient(new WebViewClient() {
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                progressBar.setVisibility(View.VISIBLE);
-
-
-            }
-
-
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-
-                progressBar.setVisibility(View.GONE);
-            }
-
-
-
-            // For 3.0+ Devices (Start)
-            // onActivityResult attached before constructor
-            protected void openFileChooser(ValueCallback uploadMsg, String acceptType)
-            {
-                mUploadMessage = uploadMsg;
-                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-                i.addCategory(Intent.CATEGORY_OPENABLE);
-                i.setType("image/*");
-                startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
-            }
-
-
-            // For Lollipop 5.0+ Devices
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
-            {
-                if (uploadMessage != null) {
-                    uploadMessage.onReceiveValue(null);
-                    uploadMessage = null;
-                }
-
-                uploadMessage = filePathCallback;
-
-                Intent intent = fileChooserParams.createIntent();
-                try
-                {
-                    startActivityForResult(intent, REQUEST_SELECT_FILE);
-                } catch (ActivityNotFoundException e)
-                {
-                    uploadMessage = null;
-                    Toast.makeText(getActivity(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
-                    return false;
-                }
-                return true;
-            }
-
-            //For Android 4.1 only
-            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
-            {
-                mUploadMessage = uploadMsg;
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
-                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
-            }
-
-            protected void openFileChooser(ValueCallback<Uri> uploadMsg)
-            {
-                mUploadMessage = uploadMsg;
-                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-                i.addCategory(Intent.CATEGORY_OPENABLE);
-                i.setType("image/*");
-                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
-            }
-        });
-
-
-        progressBar.setVisibility(View.INVISIBLE);
-    }
-
-
-    // For Lollipop 5.0+ Devices
-    public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
-
-        if (uploadMessage != null) {
-            uploadMessage.onReceiveValue(null);
-            uploadMessage = null;
-        }
-
-        uploadMessage = filePathCallback;
-        Intent intent = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            intent = fileChooserParams.createIntent();
-        }
-        try {
-            startActivityForResult(intent, REQUEST_SELECT_FILE);
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(getActivity(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
-            return false;
-        }
-        return true;
-
-
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent)
-    {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-        {
-            if (requestCode == REQUEST_SELECT_FILE)
-            {
-                if (uploadMessage == null)
-                    return;
-                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
-                uploadMessage = null;
-            }
-        }
-        else if (requestCode == FILECHOOSER_RESULTCODE)
-        {
-            if (null == mUploadMessage)
-                return;
-            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
-            // Use RESULT_OK only if you're implementing WebView inside an Activity
-            Uri result = intent == null || resultCode != getActivity().RESULT_OK ? null : intent.getData();
-            mUploadMessage.onReceiveValue(result);
-            mUploadMessage = null;
-        }
-        else
-            Toast.makeText(getActivity(), "Failed to Upload Image", Toast.LENGTH_LONG).show();
-    }
+	private final static int RC_FILE_CHOOSER = 1;
+	private WebView webview;
+	private ProgressBar progressBar;
+	private ValueCallback<Uri[]> filePathCallback;
+	
+	public FeedBackFragment() {
+		// Required empty public constructor
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == RC_FILE_CHOOSER) {
+			Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+			
+			if (result != null) {
+				filePathCallback.onReceiveValue(new Uri[]{result});
+			} else {
+				filePathCallback.onReceiveValue(null);
+			}
+		}
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_feed_back, container, false);
+		webview = view.findViewById(R.id.webviews);
+		progressBar = view.findViewById(R.id.pgbars);
+		return view;
+	}
+	
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		
+		if (getActivity() instanceof TabActivity) {
+			TabActivity tabActivity = (TabActivity) getActivity();
+			tabActivity.setActionBarTitle("Feedback");
+		}
+		
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.getSettings().setSupportZoom(true);
+		webview.getSettings().setBuiltInZoomControls(true);
+		webview.loadUrl(ApiService.FEEDBACK_PAGE);
+		progressBar.setMax(100);
+		progressBar.setProgress(1);
+		
+		webview.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public void onProgressChanged(WebView view, int progress) {
+				progressBar.setProgress(progress);
+			}
+			
+			
+			@Override
+			public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+				FeedBackFragment.this.filePathCallback = filePathCallback;
+				Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+				i.addCategory(Intent.CATEGORY_OPENABLE);
+				i.setType("image/*");
+				startActivityForResult(Intent.createChooser(i, "File Chooser"), RC_FILE_CHOOSER);
+				return true;
+			}
+		});
+		
+		webview.setWebViewClient(new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				view.loadUrl(url);
+				return true;
+			}
+			
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				super.onPageStarted(view, url, favicon);
+				progressBar.setVisibility(View.VISIBLE);
+				
+				
+			}
+			
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				progressBar.setVisibility(View.GONE);
+			}
+		});
+		
+		progressBar.setVisibility(View.INVISIBLE);
+	}
 }
--- app/src/main/java/com/beachpartnerllc/beachpartner/update/ApiService.kt
+++ app/src/main/java/com/beachpartnerllc/beachpartner/update/ApiService.kt
@@ -2,6 +2,7 @@ package com.beachpartnerllc.beachpartner.update
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 /**
@@ -9,6 +10,7 @@ import retrofit2.http.POST
  * @created on 12 Jun 2018 at 9:25 AM
  */
 interface ApiService {
+    @Headers(HEADER_NO_AUTH)
     @POST("check-app-version")
     fun checkForUpdate(@Body update: Update): Call<Update>
 
@@ -17,5 +19,8 @@ interface ApiService {
 
     companion object {
         const val URL_BASE = "https://beachpartner.com/api/"
+
+        const val NO_AUTH = "No-Auth"
+        private const val HEADER_NO_AUTH = "$NO_AUTH: true"
     }
 }
